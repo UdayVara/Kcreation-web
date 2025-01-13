75686b2da1584e480eed7a36337ef5da42a49f56
Uday vara <varaudayd@gmail.com>
2025-01-13 14:06:44 +0530

    minor ui upgrades to card

--- src/components/Custom/ItemCard.tsx
+++ src/components/Custom/ItemCard.tsx
@@ -10,20 +10,20 @@ function ItemCard({id,name,price,discount,image}:{id:string,name:string,price:nu
   const [isFav, setFav] = useState<boolean>(false);
   const router = useRouter()
   return (
-    <div className="md:w-[280px] w-[150px] lg:w-[350px] lg:h-[400px]  md:h-[350px] h-[260px] border bg-slate-50 shadow  rounded pt-2 flex flex-col" onClick={()=>{router.push(`/shop/${id}`)}}>
+    <div className="md:w-[280px] w-[150px] lg:w-[350px] lg:h-[400px]  md:h-[350px] h-[260px] min-h-max border bg-slate-50 shadow  rounded md:pt-2 flex flex-col" onClick={()=>{router.push(`/shop/${id}`)}}>
       <Image
         src={
           image?image:"https://images.pexels.com/photos/989960/pexels-photo-989960.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         }
         height={1000}
         width={1000}
-        className="w-full md:h-[250px] h-[140px] object-contain object-center "
+        className="w-full md:h-[250px] h-[140px] object-cover object-center "
         alt="Failed To load"
       />
-      <div className="body  grow md:px-3  my-2 flex flex-col mt-3">
+      <div className="body  grow md:px-0.5  my-2 flex flex-col mt-3">
         {/* <h1 className="text-sm text-neutral-400">Mens</h1> */}
         <div className="card-header flex justify-between items-start gap-2">
-          <h1 className="md:text-md md:px-4 px-2 md:text-base text-xs  grow">{name}</h1>
+          <h1 className="md:text-md md:px-4 px-1 md:text-base text-xs  grow text-wrap">{name}</h1>
           {/* <FaHeart
             className={`${
               isFav ? "text-red-600" : "text-slate-300"
@@ -33,7 +33,7 @@ function ItemCard({id,name,price,discount,image}:{id:string,name:string,price:nu
             }}
           /> */}
         </div>
-        <div className="card-body md:px-4 px-2  mt-1 self-baseline">
+        <div className="card-body md:px-4 px-1  mt-1 self-baseline">
           <h3 className="md:text-lg text-sm">
             ₹ {Math.floor(price -(discount*price)/100)}{" "}
             <span className=" line-through text-neutral-600">{price}</span>{" "}
